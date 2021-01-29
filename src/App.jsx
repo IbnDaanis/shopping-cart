70667a83cd89671210ce8b2e48c7e85abadc2fea
Ikraam <IbnDaanis@Gmail.com>
2021-01-29 19:02:01 +0000

    Init

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from 'react'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  NavLink,
+} from 'react-router-dom'
+import Home from './containers/Home'
+import Cart from './containers/Cart'
+
+const Routes = () => {
+  const [shoppingCart, setShoppingCart] = useState([])
+
+  useEffect(() => {
+    console.log({ shoppingCart })
+  }, [shoppingCart])
+  return (
+    <Router>
+      <header>
+        <NavLink to='/'>Home</NavLink>
+        <br />
+        <NavLink to='/cart'>Cart</NavLink>
+      </header>
+
+      <Switch>
+        <Route exact path='/'>
+          <Home setShoppingCart={setShoppingCart} />
+        </Route>
+        <Route exact path='/cart'>
+          <Cart shoppingCart={shoppingCart} />
+        </Route>
+      </Switch>
+    </Router>
+  )
+}
+
+export default Routes
---
+++ src/data/products.js
@@ -0,0 +1,18 @@
+export const products = [
+  {
+    name: 'Leather Backpack',
+    price: 149,
+  },
+  {
+    name: 'Commuter Briefcase',
+    price: 159,
+  },
+  {
+    name: 'Desk Mat',
+    price: 79,
+  },
+  {
+    name: 'Leather Weekender',
+    price: 149,
+  },
+]
