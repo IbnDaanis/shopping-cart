40d2809efb25b9f70af27f9c54b55780555fa16e
Ikraam <IbnDaanis@Gmail.com>
2021-02-01 06:20:17 +0000

    Added styles

--- src/containers/Product.jsx
+++ src/containers/Product.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import { products } from '../data/products'
+import '../styles/Product.scss'
 
 const Product = ({ match }) => {
-  console.log(match.params.id)
-  console.log('hi')
-  return <div>Product</div>
+  const id = match.params.id
+  console.log(products[id])
+  return (
+    <div className='product-container'>
+      <h1 className='title'>{products[id].name}</h1>
+    </div>
+  )
 }
 
 export default Product
